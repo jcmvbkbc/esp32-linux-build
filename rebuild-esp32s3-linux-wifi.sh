156f1f008525bdbcdf3ccc13b3bf462538eb9877
Max Filippov <jcmvbkbc@gmail.com>
2023-09-17 16:10:42 -0700

    rebuild-esp32s3-linux-wifi: clean up
    - move component version and configurations to the top
    - drop version from the buildroot build directory name
    - add flashing of the etc partition
    - switch to new buildroot and esp-hosted branches
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- rebuild-esp32s3-linux-wifi.sh
+++ rebuild-esp32s3-linux-wifi.sh
@@ -12,6 +12,13 @@
 
 SET_BAUDRATE='-b 2000000'
 
+CTNG_VER=xtensa-fdpic
+CTNG_CONFIG=xtensa-esp32s3-linux-uclibcfdpic
+BUILDROOT_VER=xtensa-2023.08-fdpic
+BUILDROOT_CONFIG=esp32s3_defconfig
+ESP_HOSTED_VER=ipc
+ESP_HOSTED_CONFIG=sdkconfig.defaults.esp32s3
+
 if [ ! -d autoconf-2.71/root/bin ] ; then
 	wget https://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.xz
 	tar -xf autoconf-2.71.tar.xz
@@ -25,7 +32,7 @@ export PATH=`pwd`/autoconf-2.71/root/bin:$PATH
 if [ -z "$keep_toolchain$keep_buildroot$keep_rootfs$keep_bootloader" ] ; then
 	rm -rf build
 else
-	[ -n "$keep_rootfs" ] || rm -rf build/build-xtensa-2023.02-fdpic-esp32s3
+	[ -n "$keep_rootfs" ] || rm -rf build/build-buildroot-esp32s3
 	[ -n "$keep_buildroot" ] || rm -rf build/buildroot
 	[ -n "$keep_bootloader" ] || rm -rf build/esp-hosted
 fi
@@ -46,10 +53,10 @@ export XTENSA_GNU_CONFIG=`pwd`/xtensa-dynconfig/esp32s3.so
 # toolchain
 #
 if [ ! -x crosstool-NG/builds/xtensa-esp32s3-linux-uclibcfdpic/bin/xtensa-esp32s3-linux-uclibcfdpic-gcc ] ; then
-	git clone https://github.com/jcmvbkbc/crosstool-NG.git -b xtensa-fdpic
+	git clone https://github.com/jcmvbkbc/crosstool-NG.git -b $CTNG_VER
 	pushd crosstool-NG
 	./bootstrap && ./configure --enable-local && make
-	./ct-ng xtensa-esp32s3-linux-uclibcfdpic
+	./ct-ng $CTNG_CONFIG
 	CT_PREFIX=`pwd`/builds nice ./ct-ng build
 	popd
 	[ -x crosstool-NG/builds/xtensa-esp32s3-linux-uclibcfdpic/bin/xtensa-esp32s3-linux-uclibcfdpic-gcc ] || exit 1
@@ -59,32 +66,32 @@ fi
 # kernel and rootfs
 #
 if [ ! -d buildroot ] ; then
-	git clone https://github.com/jcmvbkbc/buildroot -b xtensa-2023.02-fdpic
+	git clone https://github.com/jcmvbkbc/buildroot -b $BUILDROOT_VER
 else
 	pushd buildroot
 	git pull
 	popd
 fi
-if [ ! -d build-xtensa-2023.02-fdpic-esp32s3 ] ; then
-	nice make -C buildroot O=`pwd`/build-xtensa-2023.02-fdpic-esp32s3 esp32s3wifi_defconfig
-	buildroot/utils/config --file build-xtensa-2023.02-fdpic-esp32s3/.config --set-str TOOLCHAIN_EXTERNAL_PATH `pwd`/crosstool-NG/builds/xtensa-esp32s3-linux-uclibcfdpic
-	buildroot/utils/config --file build-xtensa-2023.02-fdpic-esp32s3/.config --set-str TOOLCHAIN_EXTERNAL_PREFIX '$(ARCH)-esp32s3-linux-uclibcfdpic'
-	buildroot/utils/config --file build-xtensa-2023.02-fdpic-esp32s3/.config --set-str TOOLCHAIN_EXTERNAL_CUSTOM_PREFIX '$(ARCH)-esp32s3-linux-uclibcfdpic'
+if [ ! -d build-buildroot-esp32s3 ] ; then
+	nice make -C buildroot O=`pwd`/build-buildroot-esp32s3 $BUILDROOT_CONFIG
+	buildroot/utils/config --file build-buildroot-esp32s3/.config --set-str TOOLCHAIN_EXTERNAL_PATH `pwd`/crosstool-NG/builds/xtensa-esp32s3-linux-uclibcfdpic
+	buildroot/utils/config --file build-buildroot-esp32s3/.config --set-str TOOLCHAIN_EXTERNAL_PREFIX '$(ARCH)-esp32s3-linux-uclibcfdpic'
+	buildroot/utils/config --file build-buildroot-esp32s3/.config --set-str TOOLCHAIN_EXTERNAL_CUSTOM_PREFIX '$(ARCH)-esp32s3-linux-uclibcfdpic'
 fi
-nice make -C buildroot O=`pwd`/build-xtensa-2023.02-fdpic-esp32s3
-[ -f build-xtensa-2023.02-fdpic-esp32s3/images/xipImage -a -f build-xtensa-2023.02-fdpic-esp32s3/images/rootfs.cramfs ] || exit 1
+nice make -C buildroot O=`pwd`/build-buildroot-esp32s3
+[ -f build-buildroot-esp32s3/images/xipImage -a -f build-buildroot-esp32s3/images/rootfs.cramfs -a -f build-buildroot-esp32s3/images/etc.jffs2 ] || exit 1
 
 #
 # bootloader
 #
-[ -d esp-hosted ] || git clone https://github.com/jcmvbkbc/esp-hosted -b shmem
+[ -d esp-hosted ] || git clone https://github.com/jcmvbkbc/esp-hosted -b $ESP_HOSTED_VER
 pushd esp-hosted/esp_hosted_ng/esp/esp_driver
 cmake .
 cd esp-idf
 . export.sh
 cd ../network_adapter
 idf.py set-target esp32s3
-cp sdkconfig.defaults.esp32s3 sdkconfig
+cp $ESP_HOSTED_CONFIG sdkconfig
 idf.py build
 read -p 'ready to flash... press enter'
 while ! idf.py $SET_BAUDRATE flash ; do
@@ -95,9 +102,9 @@ popd
 #
 # flash
 #
-parttool.py $SET_BAUDRATE write_partition --partition-name linux  --input build-xtensa-2023.02-fdpic-esp32s3/images/xipImage
-parttool.py $SET_BAUDRATE write_partition --partition-name rootfs --input build-xtensa-2023.02-fdpic-esp32s3/images/rootfs.cramfs
+parttool.py $SET_BAUDRATE write_partition --partition-name linux  --input build-buildroot-esp32s3/images/xipImage
+parttool.py $SET_BAUDRATE write_partition --partition-name rootfs --input build-buildroot-esp32s3/images/rootfs.cramfs
 if [ -z "$keep_etc" ] ; then
 	read -p 'ready to flash /etc... press enter'
-	parttool.py $SET_BAUDRATE write_partition --partition-name etc --input build-xtensa-2023.02-fdpic-esp32s3/images/rootfs.jffs2
+	parttool.py $SET_BAUDRATE write_partition --partition-name etc --input build-buildroot-esp32s3/images/etc.jffs2
 fi
